e52929d8a707691f71188419e3f645274351637b
Jacek Wydro <jacwydro@gmail.com>
2022-06-06 22:23:55 +0200

    8.2

---
+++ kodilla-exception/src/test/java/com/kodilla/exception/io/FileReaderTestSuite.java
@@ -0,0 +1,16 @@
+package com.kodilla.exception.io;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class FileReaderTestSuite {
+
+    @Test
+    void testReadFile() {
+        // given
+        FileReader fileReader = new FileReader();
+        // when & then
+        assertDoesNotThrow(() -> fileReader.readFile());
+    }
+}
