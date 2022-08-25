4126bfab9ec6023b5fa738617e8cfc58c400189f
jacolec <jacwydro@gmail.com>
2022-08-25 21:22:02 +0200

    16.6

--- kodilla-jdbc/src/test/java/DbManagerTestSuite.java
+++ kodilla-jdbc/src/test/java/DbManagerTestSuite.java
@@ -2,7 +2,9 @@ import com.kodilla.jdbc.DbManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class DbManagerTestSuite {
 
@@ -13,5 +15,48 @@ public class DbManagerTestSuite {
         DbManager dbManager = DbManager.getInstance();
         //then
         Assertions.assertNotNull(dbManager.getConnection());
+    }
+
+    @Test
+    void testSelectUsers() throws SQLException {
+        //given
+        DbManager dbManager = DbManager.getInstance();
+        //when
+        String sqlQuery = "select * from USERS";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet rs = statement.executeQuery(sqlQuery);
+        //then
+        int counter = 0;
+        while (rs.next()) {
+            System.out.println(rs.getInt("ID") + ", " +
+                    rs.getString("FIRSTNAME") + ", " +
+                    rs.getString("LASTNAME"));
+            counter++;
+        }
+        rs.close();
+        statement.close();
+        Assertions.assertEquals(5, counter);
+    }
+
+    @Test
+    void testSelectUsersAndPosts() throws SQLException {
+        //given
+        DbManager dbManager = DbManager.getInstance();
+        //when
+        String postsQuery = "select U.FIRSTNAME, U.LASTNAME, count(*) as POSTS_NUMBER from USERS U join POSTS P on U.ID = P.USER_ID group by P.USER_ID having count(*) >= 2";
+        Statement statement = dbManager.getConnection().createStatement();
+        ResultSet resultSet = statement.executeQuery(postsQuery);
+        //then
+        int i = 0;
+        while (resultSet.next()) {
+            System.out.println(resultSet.getString("FIRSTNAME") + ", " +
+                    resultSet.getString("LASTNAME") + ", " +
+                            resultSet.getInt("POSTS_NUMBER"));
+            i++;
+        }
+        resultSet.close();
+        statement.close();
+        Assertions.assertEquals(1, i);
+
     }
 }
